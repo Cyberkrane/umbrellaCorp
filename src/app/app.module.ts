7b68a3fbba6d43c1a2fed7f29e3e6db8fd31661f
Rolando Corral <cyberkrane@gmail.com>
2024-02-29 10:12:56 -0300

    el reinicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthRoutingModule } from './auth/auth-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AuthRoutingModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
